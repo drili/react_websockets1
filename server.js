762b8650ccf002859e93c20e80909f1917bccd25
drili <DriliB@hotmail.com>
2023-10-03 14:02:49 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const http = require("http")
+const WebSocket = require("ws")
+
+const app = express()
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        // *** Broadcast the message to all connected clients
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message)
+            }
+        })
+    })
+})
+
+server.listen(3001, () => {
+    console.log(`::: Websocket server is running on port 3001`);
+})
